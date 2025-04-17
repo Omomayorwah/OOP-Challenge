993d2b1e4d8db2fc2bf8686539b965d5a4ea3df5
Omomayorwah <kintunde2010@gmail.com>
2025-04-17 21:18:21 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+my_pet = Pet("Maxel")
+my_pet.get_status()    # To show initial status 
+my_pet.eat()           # Calling the eat() method 
+my_pet.sleep()         # Calling the sleep() method 
+my_pet.play()          # Calling the play() method 
+my_pet.get_status()    # To view status after calling the methods
+
+my_pet = Pet("Maxel")
+my_pet.train("sit")
+my_pet.train("roll over")
+my_pet.train("play dead")
+my_pet.show_tricks()
