eee86e20cdecf37b89eb87320a1efdefdeed9917
Omomayorwah <kintunde2010@gmail.com>
2025-04-18 09:24:12 +0100

    structured my code

--- Pet.py
+++ Pet.py
@@ -1,10 +1,10 @@
 class Pet:
-  def __init__(self, name):
+    def __init__(self, name):
         self.name = name
-        self.hunger = 5        # setting default mid-level
-        self.energy = 5        # setting default mid-level
-        self.happiness = 5     # setting default mid-level
-        self.tricks = []       # List to store learned tricks
+        self.hunger = 5        
+        self.energy = 5        
+        self.happiness = 5    
+        self.tricks = []       
 
     def eat(self):
         self.hunger = max(0, self.hunger - 3)
@@ -28,11 +28,10 @@ class Pet:
             for trick in self.tricks:
                 print(f" - {trick}")
             else:
-            print(f"{self.name} hasn't learned any tricks yet.")
+                print(f"{self.name} hasn't learned any tricks yet.")
 
     def get_status(self):
         print(f"Pet Name: {self.name}")
         print(f"Hunger: {self.hunger} of 10")
         print(f"Energy: {self.energy} of 10")
         print(f"Happiness: {self.happiness} of 10")
-
--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 my_pet = Pet("Maxel")
-my_pet.get_status()    # To show initial status 
-my_pet.eat()           # Calling the eat() method 
-my_pet.sleep()         # Calling the sleep() method 
-my_pet.play()          # Calling the play() method 
-my_pet.get_status()    # To view status after calling the methods
+my_pet.get_status()         # To show initial status 
+
+my_pet.eat()                # Calling the eat() method 
+my_pet.sleep()              # Calling the sleep() method 
+my_pet.play()               # Calling the play() method 
+my_pet.get_status()         # To view status after calling the methods
 
 my_pet = Pet("Maxel")
-my_pet.train("sit")
-my_pet.train("roll over")
-my_pet.train("play dead")
-my_pet.show_tricks()
+my_pet.train("sit")         # Calling the train() method / training the pet to sit
+my_pet.train("roll over")   # Calling the train() method / training the pet to roll over
+my_pet.train("play dead")   # Calling the train() method / training the pet to play dead
+my_pet.show_tricks()        # To show the tricks learned by the pet
